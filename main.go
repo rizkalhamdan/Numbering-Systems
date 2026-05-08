43928baedc962820b3f306877628fc8760d19b1f
RIZK <rizkalhamdan@gmail.com>
2026-05-08 21:43:25 +0300

    Got it

--- main.go
+++ main.go
@@ -2,21 +2,56 @@ package main
 
 // import "RIZK/numbering-systems/utils"
 
-
-
 import (
+	"fmt"
 	"RIZK/numbering-systems/decimal"
 	"RIZK/numbering-systems/utils"
+	
 )
 
 func main() {
 	decimal.Test()
 
+	tryit := utils.GetRealNumber("7f")
+
+
+
+	fmt.Println(tryit)
+
+
+	var choise int
+	var pri string
+	var res []int
+
 	for {
-		utils.DO("clear")
+		// utils.DO("clear")
+		res = res[:0]
 
-		println(" ## Number System ##")
+		// welcome
+		utils.Welcome()
 
+		// get type id
+		utils.PrintTypes()
+		println("Your choise: ")
+
+		choise = 0
+		fmt.Scan(&choise)
+
+		// get the number
+		println("Enter the number: ")
+		fmt.Scan(&pri)
 		
+
+
+		// do switch type
+		switch choise {
+			case 1: {
+				
+				break;
+			}
+
+		}
+
+	
 	}
 }
--- utils/public.go
+++ utils/public.go
@@ -17,4 +17,94 @@ func DO(com string) bool {
 	cmd.Run()
 
 	return true
-} 
+}
+
+func Welcome() {
+	println(" ## Number System ##")
+
+	println("Good morning!")
+
+	println()
+
+	println("##################")
+	println("##################")
+}
+
+func PrintTypes() {
+	println()
+
+	println("Numbering Systems")
+
+	println("\t1. Binary")
+	println("\t2. Octa")
+	println("\t3. Decimal")
+	println("\t4. Hexadecimal")
+
+	println()
+}
+
+
+
+
+// ...existing code...
+func GetRealNumber(num string) []int {
+
+    var new_arr []int
+
+    for _, r := range num {
+        switch {
+        case r >= '0' && r <= '9':
+            new_arr = append(new_arr, int(r-'0'))
+        case r >= 'a' && r <= 'f':
+            new_arr = append(new_arr, int(r-'a'+10))
+        case r >= 'A' && r <= 'F':
+            new_arr = append(new_arr, int(r-'A'+10))
+        default:
+            // ignore non-hex characters
+        }
+    }
+
+    return new_arr
+
+}
+// ...existing code...
+
+
+
+
+
+// func GetRealNumber(num string) []int {
+
+// 	num = strings.ToLower(num)
+// 	arr := strings.Split(num, "")
+// 	var new_arr []int
+
+// 	for _, val := range arr {
+
+// 		switch val {
+// 		case "a":
+// 			new_arr = append(new_arr, 10)
+// 		case "b":
+// 			new_arr = append(new_arr, 11)
+// 		case "c":
+// 			new_arr = append(new_arr, 12)
+// 		case "d":
+// 			new_arr = append(new_arr, 13)
+// 		case "e":
+// 			new_arr = append(new_arr, 14)
+// 		case "f":
+// 			new_arr = append(new_arr, 15)
+// 		default:
+// 			nve, err := strconv.Atoi(val)
+
+// 			if err == nil {
+// 				break
+// 			}
+
+// 			new_arr = append(new_arr, nve)
+// 		}
+// 	}
+
+// 	return new_arr
+
+// }
